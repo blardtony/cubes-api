7f2bfce2284acf626bd02c0fa3fd792b27ebca9c
Tony <tonyblard55@gmail.com>
2021-03-21 18:23:11 +0100

    Create Challenge and Synopsis data

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Synopsis;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
+use App\Entity\Challenge;
 
 
 class UserFixtures extends Fixture
@@ -25,9 +27,35 @@ class UserFixtures extends Fixture
                 $user,
                 'the_new_password'
             ));
-
+            $user->setFirstname(sprintf("firstname %d", $i));
+            $user->setName(sprintf("last name %d", $i));
+            $user->setBirthday(new \DateTimeImmutable('1998-09-16 00:00'));
             $manager->persist($user);
         }
+        $challenge = new Challenge();
+        $challenge->setContent("Content TEST");
+        $challenge->setBonus("Bonus");
+        $challenge->setShare(0);
+        $challenge->setSuspend(0);
+        $challenge->setTitle("title challenge");
+        $challenge->setValidate(1);
+
+        $synopsis = new Synopsis();
+        $synopsis->setShare(0);
+        $synopsis->setSuspend(0);
+        $synopsis->setTitle("title synopsis");
+        $synopsis->setValidate(1);
+        $synopsis->setContent("Content fiche lecture");
+        $synopsis->setBirthdayAuthor(new \DateTimeImmutable('1985-09-16 00:00'));
+        $synopsis->setAuthor("Auteur");
+        $synopsis->setDeathAuthor(new \DateTimeImmutable('2010-09-16 00:00'));
+        $synopsis->setGenre("Genre");
+        $synopsis->setLiteraryMovement("Mouvement litérraire");
+        $synopsis->setOpinion("Mauvais");
+        $synopsis->setPublishDate(new \DateTimeImmutable('2000-09-16 00:00'));
+
+        $manager->persist($challenge);
+        $manager->persist($synopsis);
         $manager->flush();
 
     }

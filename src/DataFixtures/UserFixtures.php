1fa1ceff859b446a6815d8bf4a9616434fec1527
Tony <tonyblard55@gmail.com>
2021-04-24 13:21:29 +0200

    Create fake data

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Citizen;
 use App\Entity\Synopsis;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -19,8 +21,12 @@ class UserFixtures extends Fixture
     }
     public function load(ObjectManager $manager)
     {
+        $category = new Category();
+        $category->setName("Categorie");
+        $manager->persist($category);
+
         for ($i=0; $i <=2 ; $i++) {
-            $user = new User();
+            $user = new Citizen();
             $user->setEmail(sprintf("email%d@gmail.com", $i));
 
             $user->setPassword($this->PasswordEncode->encodePassword(
@@ -31,31 +37,37 @@ class UserFixtures extends Fixture
             $user->setName(sprintf("last name %d", $i));
             $user->setBirthday(new \DateTimeImmutable('1998-09-16 00:00'));
             $manager->persist($user);
+
+
+            $challenge = new Challenge();
+            $challenge->setContent("Content TEST");
+            $challenge->setBonus("Bonus");
+            $challenge->setShare(0);
+            $challenge->setSuspend(0);
+            $challenge->setTitle("title challenge");
+            $challenge->setValidate(1);
+            $challenge->setCategory($category);
+            $challenge->setAuthor($user);
+
+            $synopsis = new Synopsis();
+            $synopsis->setShare(0);
+            $synopsis->setSuspend(0);
+            $synopsis->setTitle("title synopsis");
+            $synopsis->setValidate(1);
+            $synopsis->setCategory($category);
+            $synopsis->setContent("Content fiche lecture");
+            $synopsis->setBirthdayAuthor(new \DateTimeImmutable('1985-09-16 00:00'));
+            $synopsis->setAuthorBook("Auteur");
+            $synopsis->setDeathAuthor(new \DateTimeImmutable('2010-09-16 00:00'));
+            $synopsis->setGenre("Genre");
+            $synopsis->setLiteraryMovement("Mouvement litérraire");
+            $synopsis->setOpinion("Mauvais");
+            $synopsis->setPublishDate(new \DateTimeImmutable('2000-09-16 00:00'));
+            $synopsis->setAuthor($user);
+            $manager->persist($challenge);
+            $manager->persist($synopsis);
         }
-        $challenge = new Challenge();
-        $challenge->setContent("Content TEST");
-        $challenge->setBonus("Bonus");
-        $challenge->setShare(0);
-        $challenge->setSuspend(0);
-        $challenge->setTitle("title challenge");
-        $challenge->setValidate(1);
 
-        $synopsis = new Synopsis();
-        $synopsis->setShare(0);
-        $synopsis->setSuspend(0);
-        $synopsis->setTitle("title synopsis");
-        $synopsis->setValidate(1);
-        $synopsis->setContent("Content fiche lecture");
-        $synopsis->setBirthdayAuthor(new \DateTimeImmutable('1985-09-16 00:00'));
-        $synopsis->setAuthor("Auteur");
-        $synopsis->setDeathAuthor(new \DateTimeImmutable('2010-09-16 00:00'));
-        $synopsis->setGenre("Genre");
-        $synopsis->setLiteraryMovement("Mouvement litérraire");
-        $synopsis->setOpinion("Mauvais");
-        $synopsis->setPublishDate(new \DateTimeImmutable('2000-09-16 00:00'));
-
-        $manager->persist($challenge);
-        $manager->persist($synopsis);
         $manager->flush();
 
     }

962b31769c4ba1f80d24f6a51f4afa31e450ecd1
Tony <tonyblard55@gmail.com>
2021-04-25 14:07:27 +0200

    Remove commentary and change method's name

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Challenge;
 use App\Repository\ChallengeRepository;
-use App\Repository\RessourceRepository;
-use App\Repository\SynopsisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -19,16 +19,18 @@ class ChallengeController extends AbstractController
      * @param ChallengeRepository $challengeRepository
      * @return JsonResponse
      */
-    public function getInfos(SerializerInterface $serializer, ChallengeRepository $challengeRepository): JsonResponse
+    public function getAllChallenges(SerializerInterface $serializer, ChallengeRepository $challengeRepository): JsonResponse
     {
         $challenges = $challengeRepository->findAll();
-        //dd($challenges);
-        //dd( $serializer->serialize($challenges, 'json', ['groups' => 'ressource:get']));
+
         return new JsonResponse(
             $serializer->serialize($challenges, 'json', ['groups' => 'ressource:get']),
             200,
             [],
             true
         );
+
     }
+
+
 }

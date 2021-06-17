ff7f9f35716821084b6e82a7b453f5826a8ff1d8
Tony <tonyblard55@gmail.com>
2021-06-17 15:21:14 +0200

    Test unitaire : Créer une ressource et des utilisateurs

---
+++ tests/RessourceUnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Challenge;
+use PHPUnit\Framework\TestCase;
+
+class RessourceUnitTest extends TestCase
+{
+    public function testCreationArticle(): void
+    {
+        $ressource = new Challenge();
+        $category =
+
+        $ressource->setTitle()
+        ->setContent()
+        ->setCategory();
+    }
+}
--- tests/UserUnitTest.php
+++ tests/UserUnitTest.php
@@ -2,12 +2,48 @@
 
 namespace App\Tests;
 
+use App\Entity\Admin;
+use App\Entity\Citizen;
+use DateTime;
 use PHPUnit\Framework\TestCase;
-
 class UserUnitTest extends TestCase
 {
-    public function testSomething(): void
+
+    public function testCreationCompteCitoyen(): void
     {
-        $this->assertTrue(true);
+        $user = new Citizen();
+
+        $user->setFirstname("Prénom")
+            ->setName("Nom")
+            ->setEmail("user@email.com")
+             ->setPassword(
+                 'the_new_password'
+             )
+            ->setBirthday(new \DateTimeImmutable("1990-03-23T00:00:00+02:00"));
+
+        $this->assertTrue($user->getEmail() === "user@email.com");
+        $this->assertTrue($user->getName() === "Nom");
+        $this->assertTrue($user->getFirstname() === "Prénom");
+        $this->assertTrue($user->getPassword() === "the_new_password");
+        $this->assertTrue($user->getRoles() === ["ROLE_USER"]);
+    }
+
+    public function testCreationCompteAdmin(): void
+    {
+        $admin = new Admin();
+
+        $admin->setFirstname("Prénom")
+            ->setName("Nom")
+            ->setEmail("user@email.com")
+            ->setPassword(
+                'the_new_password'
+            )
+            ->setBirthday(new \DateTimeImmutable("1990-03-23T00:00:00+02:00"));
+
+        $this->assertTrue($admin->getEmail() === "user@email.com");
+        $this->assertTrue($admin->getName() === "Nom");
+        $this->assertTrue($admin->getFirstname() === "Prénom");
+        $this->assertTrue($admin->getPassword() === "the_new_password");
+        $this->assertTrue($admin->getRoles() === ["ROLE_ADMIN", "ROLE_USER"]);
     }
 }
